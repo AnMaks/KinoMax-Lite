6a1209daf2f333ee13299a29d1c0095ce9646bb5
AnMaks <anmaks@gmail.com>
2025-11-09 14:01:22 +0200

    Онбновлены конфигураторы подключения View

---
+++ kernel/Exceptions/ViewNotFoundException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Kernal\Exceptions;
+
+
+
+class ViewNotFoundException extends \Exception
+{
+    
+}
--- kernel/View/View.php
+++ kernel/View/View.php
@@ -8,6 +8,20 @@ class View
 {
     public function page(string $name): void
     {
+
+        $viewPath = APP_PATH . "/views/pages/$name.php";
+
+        if(!file_exists($viewPath)){
+            throw new \Exception("View $name not found");
+        }
+
+        extract(['view' => $this]);
+
         include_once APP_PATH . "/views/pages/$name.php";
+    }
+
+    public function component(string $name): void
+    {
+        include_once APP_PATH . "/views/components/$name.php";
     }
 }
